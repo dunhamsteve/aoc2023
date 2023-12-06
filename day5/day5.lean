931a57823c8fd02e67d74a871160a87a674f9844
Steve Dunham <dunhamsteve@gmail.com>
2023-12-05 20:29:17 -0800

    termination proof for day5 (sans a theorem from std4)

--- day5/day5.lean
+++ day5/day5.lean
@@ -2,9 +2,6 @@ import Lean
 import Lean.Data.Parsec
 open Lean.Parsec
 
--- I'll need ranges, but the maps are the same size...
--- let's parse first.
-
 structure MapEntry where
   dest : Nat
   src  : Nat
@@ -50,31 +47,42 @@ def part1 (p : Problem) : IO Unit := do
 
 abbrev Range := Nat × Nat
 
+-- this is in std4 library
+theorem Nat.sub_lt_right_of_lt_add {n k m : Nat} (H : n ≤ k) (h : k < m + n) : k - n < m :=
+  sorry
+
 -- assumes sorted
-partial
 def apply' (r : Range) : List MapEntry -> List Range
 | [] => [r]
 | x@(⟨ d,s,l ⟩ :: es) =>
-     if r.1 + r.2 <= s then [r] -- map after range
-     else if s + l <= r.1 then apply' r es -- map before range
+     if h1 : r.2 + r.1 <= s then [r] -- map after range
+     else if h2 : s + l <= r.1 then apply' r es -- map before range
      -- take off any bare range on front
-     else if r.1 < s then (r.1, s - r.1) :: apply' (s, r.2 + r.1 - s) x
+     else if h3 : r.1 < s then
+        have : r.2 + r.1 - s < r.2 := by
+            apply Nat.sub_lt_right_of_lt_add
+            exact Nat.le_of_lt (Nat.gt_of_not_le h1)
+            apply Nat.add_lt_add_left
+            assumption
+        (r.1, s - r.1) :: apply' (s, r.2 + r.1 - s) x
      -- we have a match
-     else if s + l < r.1 + r.2 then
+     else if h4 : s + l < r.1 + r.2 then
+        have : r.2 + (r.1 - s) - l < r.2 := by
+          rw [<-Nat.add_sub_assoc, Nat.sub_sub]
+          apply Nat.sub_lt_right_of_lt_add
+          simp [Nat.le_of_lt,Nat.add_comm,h4]
+          apply Nat.add_lt_add_left
+          exact Nat.gt_of_not_le h2
+          exact Nat.ge_of_not_lt h3
         let slack := r.1 - s
         (r.1 + d - s, l - slack) :: apply' (r.1 + l - slack, r.2 + slack - l) x
      else [(r.1 + d - s, r.2)]
+termination_by apply' r m => (r.2, m)
 
 def apply (ranges : List Range) (entries : List MapEntry) :=
   let entries := (entries.toArray.qsort (·.2 < ·.2)).toList
-  --dbg_trace "# apply {repr entries}"
-  let apply'' r me :=
-    let r' := apply' r me
-    --dbg_trace "##  {r} -> {r'}"
-    r'
-  let res := (ranges.map (apply'' · entries)).join
-  --dbg_trace "{ranges} -> {res}"
-  res
+  let apply'' r me := apply' r me
+  (ranges.map (apply'' · entries)).join
 
 def mkRanges : List Nat -> Option (List Range)
 | a :: b :: rs => mkRanges rs >>= ((a,b) :: ·)
