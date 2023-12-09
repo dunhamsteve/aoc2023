d3277273bf0a1735337ecb096b40c63001ecea18
Steve Dunham <dunhamsteve@gmail.com>
2023-12-09 09:22:02 -0800

    Day 9 totality

--- day9/day9.lean
+++ day9/day9.lean
@@ -8,20 +8,40 @@ def diff : List Int -> List Int
 | a :: b :: rest => (b - a) :: diff (b :: rest)
 | _ => []
 
+theorem diff_smaller : ∀ (xs : List Int) (n : Nat),
+  (h : xs.length = .succ n) -> (diff xs).length < xs.length := by
+  intros xs
+  induction xs
+  intro n h
+  contradiction
+  rename_i tail ih
+  intro n h
+  match tail with
+  | .nil => apply Nat.zero_lt_succ
+  | x :: tail =>
+          apply Nat.succ_lt_succ
+          apply ih tail.length rfl
+
 def zeros : (List Int) -> Bool
 | [] => true
 | 0 :: rest => zeros rest
 | _ => false
 
-partial
 def next (data : List Int) :=
   if zeros data then 0
-  else data.getLast! + next (diff data)
+  else match h : data with
+  | x :: xs => data.getLast (by simp [h]) + next (diff data)
+  | _ => 0
+termination_by next data => data.length
+decreasing_by apply diff_smaller; rw [h]; rfl
 
-partial
 def prev (data : List Int) :=
   if zeros data then 0
-  else data.head! - prev (diff data)
+  else match h : data with
+  | x :: xs => data.head (by simp [h]) - prev (diff data)
+  | _ => 0
+termination_by prev data => data.length
+decreasing_by apply diff_smaller; rw [h]; rfl
 
 def sum : List Int -> Int := List.foldl (·+·) 0
 
