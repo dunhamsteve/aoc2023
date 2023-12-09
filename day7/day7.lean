36b917c74d361cec0a44b3eaa9f35e92dc2428f2
Steve Dunham <dunhamsteve@gmail.com>
2023-12-09 11:25:21 -0800

    Day 7 totality

--- day7/day7.lean
+++ day7/day7.lean
@@ -5,18 +5,19 @@ open Lean.Parsec
 def order := "23456789TJQKA".toList.toArray
 def jorder := "J23456789TQKA".toList.toArray
 
-abbrev Hand := List Nat
+abbrev Hand := Nat × List Nat
 
 def mkHand (part1 : Bool) (cards : String) : Option Hand := do
   let order := ite part1 order jorder
-  let kinds <- cards.toList.mapM (λ c => (order.indexOf? c : Option Nat))
-  let mut counts := mkArray order.size 0
+  let kinds <- cards.toList.mapM (λ c => (order.indexOf? c ))
+  let mut counts : { ar : Array Int // ar.size = order.size } := ⟨ mkArray order.size 0, by simp ⟩
   for rk in kinds do
-    counts := counts.set! rk (counts[rk]! + 1)
+    have : rk.val < counts.val.size := by simp [rk.2,counts.2]
+    counts := ⟨ counts.1.set ⟨rk.val, this⟩ (counts.1[rk] + 1), by simp [counts.2] ⟩
   let mut type := 0
   let start := ite part1 0 1
-  for h:i in [start:counts.size] do
-    let c := counts[i]'h.2
+  for h:i in [start:counts.1.size] do
+    let c := counts.1[i]'h.2
     if c = 5 then type := 6
     if c = 4 then type := 5
     if c = 3 then if type = 1 then type := 4 else type := 3
@@ -26,14 +27,14 @@ def mkHand (part1 : Bool) (cards : String) : Option Hand := do
         else if type = 1 then type := 2
         else type := 1
   if ! part1 then
-    type := match counts[0]!, type with
+    type := match counts.1.getD 0 0, type with
       | 5, _ => 6
       | 4, _ => 6
       | 3, 1 => 6 | 3, _ => 5
       | 2, 3 => 6 | 2, 1 => 5 | 2, _ => 3
       | 1, 5 => 6 | 1, 3 => 5 | 1, 2 => 4 | 1, 1 => 3 | 1, _ => 1
       | _, _ => type
-  .some (type :: kinds)
+  .some (type , kinds.map (·.1))
 
 abbrev Line := String × Nat
 
@@ -46,7 +47,7 @@ def parseFile (content : String) : Option (List Line) :=
   let lines := content.trim.splitOn "\n"
   lines.mapM parseLine
 
-def lt (a b : List Nat) : Bool := a < b
+def lt (a b : Hand) : Bool := a.1 < b.1 || a.1 == b.1 && a.2 < b.2
 
 def run (lines : List Line) (part1 : Bool) : IO Unit := do
   let .some hands := lines.mapM (fun (a,b) => (·,b) <$> mkHand part1 a) | println! "mkHand fail"
