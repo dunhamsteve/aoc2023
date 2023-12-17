0cac8d64f16fe27efde2de262bd605e65ca0f28f
Steve Dunham <dunhamsteve@gmail.com>
2023-12-16 20:46:32 -0800

    prove indices for day 16

--- day16/day16.lean
+++ day16/day16.lean
@@ -4,12 +4,37 @@ structure Grid (α : Type) where
   data : Array α
   width : Nat
   height : Nat
-deriving Repr, Inhabited
+  pf : data.size = width.succ * height
+deriving Repr
+
+instance [Inhabited α ] : Inhabited (Grid α) where
+  default := ⟨ mkArray 0 default , 0, 0, rfl ⟩
+
+theorem grid_ix {r c width height : Nat} (h : r < height ∧ c < width) : width.succ * r + c < width.succ*height := by
+  --let ix := r*width.succ + c
+  have h2 : width.succ * r.succ ≤width.succ * height := by
+    apply Nat.mul_le_mul_left
+    exact h.1
+  apply Nat.lt_of_lt_of_le _ h2
+  rw [Nat.mul_succ]
+  apply Nat.add_lt_add_left
+  apply Nat.lt_trans h.2
+  apply Nat.lt_succ_self
 
 namespace Grid
-  def get [Inhabited α] (p : Grid α) (r c : Nat) := p.data[r*p.width.succ + c]!
+  def get [Inhabited α] (p : Grid α) (r c : Nat) :=
+      if h : r < p.height ∧ c < p.width then
+        let ix := p.width.succ * r + c
+        have : ix < p.data.size := by simp [p.pf,h,grid_ix]
+        p.data[ix]
+    else default
+
   def set (p : Grid α) (r c : Nat) (val : α) :=
-      { p with data := p.data.set! (r*p.width.succ + c) val }
+    if h : r < p.height ∧ c < p.width then
+      let ix := p.width.succ * r + c
+      have : ix < p.data.size := by simp [p.pf,h,grid_ix]
+      { p with data := p.data.set ⟨ix, this⟩  val, pf := by simp [p.pf] }
+    else p
   def mark (p : Grid Nat) (r c fl : Nat) :=
     p.set r c (p.get r c ||| fl)
   def check (p : Grid Nat) (r c fl : Nat) : Bool :=
@@ -21,13 +46,16 @@ namespace Grid
 end Grid
 
 def parse (content : String) : Option (Grid Char):= do
-  let content := content.trim
-  let lines := content.splitOn "\n"
+  let content := content
+  let lines := content.trim.splitOn "\n"
   let height := lines.length
   let .some width := lines.head?.map (·.length) | .none
-  if height*width.succ - 1 != content.length then
-    panic! "*** {content.length} {height} {width} {height*(width+1)}"
-  .some ⟨ content.toList.toArray, width, height ⟩
+  let data := content.toList.toArray
+  if h : data.size = width.succ*height then
+    .some ⟨ content.toList.toArray, width, height, h ⟩
+  else
+    dbg_trace "*** {content.length} {height} {width} {height*(width+1)}"
+    .none
 
 -- North 1
 -- East 2
@@ -47,6 +75,8 @@ def Grid.next (p : Grid Char) (l : Light) (ls : List Light) : List Light := matc
   | ⟨ r, c, 8 ⟩ => ite (c > 0) (⟨ r, c.pred, 8⟩ :: ls) ls
   | ⟨ _, _, _ ⟩ => dbg_trace "BAD dir {repr l}"; []
 
+-- this is decreasing on number of empty marks and length of ls, but I think that
+-- would be a pain to show.
 partial
 def Grid.stepLight (p : Grid Char) (marks : Grid Nat) : List Light -> Nat
 | [] => (marks.data.filter λ x => x > 0).size
@@ -70,7 +100,7 @@ def Grid.stepLight (p : Grid Char) (marks : Grid Nat) : List Light -> Nat
         | _,    _  => dbg_trace "FAIL {repr l} {p.get r c}"; 0
 
 def Grid.zeros (p : Grid α) : Grid Nat :=
-  ⟨ mkArray (p.width.succ*p.height) 0, p.width, p.height ⟩
+  ⟨ mkArray (p.width.succ*p.height) 0, p.width, p.height, by simp [p.pf] ⟩
 
 def best (p : Grid Char) : Nat := Id.run do
   let mut result := 0
